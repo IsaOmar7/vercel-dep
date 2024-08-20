23b9a65732b99c351b366ecb53d006fa062cef84
IsaOmar7 <omar.is1207@gmail.com>
2024-08-20 18:48:36 +0300

    sad

--- server/index.js
+++ server/index.js
@@ -88,15 +88,16 @@ app.post('/fetch-metadata', async (req, res) => {
   }
 });
 
-app.use(express.static(path.join(__dirname, "..",'client', 'build')));
+// app.use(express.static(path.join(__dirname, "..",'client', 'build')));
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, "..",'client', 'build', 'index.html'));
-});
+// // The "catchall" handler: for any request that doesn't
+// // match one above, send back React's index.html file.
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, "..",'client', 'build', 'index.html'));
+// });
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
+const PORT =  5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
