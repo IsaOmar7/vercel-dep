07da8395cd269390b6e2f7f5ea4456ddc7a8b2bd
IsaOmar7 <omar.is1207@gmail.com>
2024-08-20 19:38:36 +0300

    asasasas

--- server/index.js
+++ server/index.js
@@ -96,7 +96,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, "..",'file-client', 'build', 'index.html'));
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT =  5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
